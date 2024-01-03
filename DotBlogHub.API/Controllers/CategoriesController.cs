058b3d9bcfd73269b708f8e67bf2b5689369e140
theshameem <theshameem.alam@gmail.com>
2024-01-03 16:46:04 +0600

    feat: db migrations add

---
+++ DotBlogHub.API/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotBlogHub.API.Controllers
+{
+	//https://localhost:xxxx/api/categories
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+
+	}
+}
--- DotBlogHub.API/Program.cs
+++ DotBlogHub.API/Program.cs
@@ -1,3 +1,6 @@
+using DotBlogHub.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DotBlogHubConnectionString"))
+) ;
 
 var app = builder.Build();
 
