6536c169efbd0178942464dd6b035f54150479bc
theshameem <theshameem.alam@gmail.com>
2024-02-06 20:57:20 +0600

    feat: blog post repository create blog implementation

---
+++ DotBlogHub.API/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,24 @@
+﻿using DotBlogHub.API.Data;
+using DotBlogHub.API.Models.Domain;
+using DotBlogHub.API.Repositories.Interface;
+
+namespace DotBlogHub.API.Repositories.Implementation
+{
+	public class BlogPostRepository: IBlogPostRepository
+	{
+		private readonly ApplicationDbContext dbContext;
+
+		public BlogPostRepository(ApplicationDbContext dbContext)
+        {
+			this.dbContext = dbContext;
+		}
+
+		public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+		{
+			await dbContext.BlogPosts.AddAsync(blogPost);
+			await dbContext.SaveChangesAsync();
+
+			return blogPost;
+		}
+    }
+}
