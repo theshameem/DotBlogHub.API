7204b2bd54ae56f592ddfb3b8e1feb6eb1aab4ee
theshameem <theshameem.alam@gmail.com>
2024-02-07 07:08:59 +0600

    fix: featuredImageUrl db migration

---
+++ DotBlogHub.API/Migrations/20240207010519_Blog Post FeturedImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotBlogHub.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogPostFeturedImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeatureImageUrl",
+                table: "BlogPosts",
+                newName: "FeaturedImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeaturedImageUrl",
+                table: "BlogPosts",
+                newName: "FeatureImageUrl");
+        }
+    }
+}
