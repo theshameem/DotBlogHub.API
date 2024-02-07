86224d6b55a45504e97efa90d6cf2044e8983085
theshameem <theshameem.alam@gmail.com>
2024-02-07 19:59:43 +0600

    feat: get blog list interface

--- DotBlogHub.API/Repositories/Interface/IBlogPostRepository.cs
+++ DotBlogHub.API/Repositories/Interface/IBlogPostRepository.cs
@@ -5,5 +5,6 @@ namespace DotBlogHub.API.Repositories.Interface
 	public interface IBlogPostRepository
 	{
 		Task<BlogPost> CreateAsync(BlogPost blogPost);
+		Task<IEnumerable<BlogPost>> GetAllBlogsAsync(); 
 	}
 }
